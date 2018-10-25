6d3ca925cfa5f5743126c8217dd30f03e1df014c
0xiaobao0 <1920566573@qq.com>
2018-10-25 18:15:44 +0800

    在view中导入modles中定义的UserMessage类，并对post请求做表单保存操作
    Signed-off-by: 0xiaobao0 <1920566573@qq.com>

--- apps/message/views.py
+++ apps/message/views.py
@@ -1,6 +1,23 @@
+# coding=utf-8
 from django.shortcuts import render
+from .models import UserMessage
+
 
 # Create your views here.
 
 def getform(request):
+    # all_messages = UserMessage.objects.filter(name = 'mrliu')
+    # for message in all_messages:
+    #     print message.email
+    if request.method == 'POST':
+        usermessage = UserMessage()
+        usermessage.name = request.POST.get('name', '')
+        usermessage.email = request.POST.get('email', '')
+        usermessage.address = request.POST.get('address', '')
+        usermessage.message = request.POST.get('message', '')
+        if(usermessage.name and usermessage.email and usermessage.address and usermessage.message):
+            usermessage.save()
+        else:
+            return render(request, 'userform.html')
+
     return render(request, 'userform.html')
