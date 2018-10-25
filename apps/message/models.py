10519d2722a02430fc9b6cb3a4fe9521acbe5bc6
0xiaobao0 <1920566573@qq.com>
2018-10-25 18:14:25 +0800

    添加编码格式，定义一个UserMessage类，在类中定义需要生成表的字段
    Signed-off-by: 0xiaobao0 <1920566573@qq.com>

--- apps/message/models.py
+++ apps/message/models.py
@@ -1,5 +1,17 @@
+# coding=utf-8
 from __future__ import unicode_literals
 
 from django.db import models
 
 # Create your models here.
+class UserMessage(models.Model):
+    name = models.CharField(max_length=20, null=True, blank=True, default="", verbose_name=u"姓名")
+    email = models.EmailField(verbose_name=u"邮箱")
+    address = models.CharField(max_length=50, verbose_name=u"地址")
+    message = models.CharField(max_length=1000, verbose_name=u"留言")
+
+    class Meta:
+        verbose_name = u"用户信息"
+        verbose_name_plural = verbose_name
+
+
